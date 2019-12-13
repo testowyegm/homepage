deaeba5c39ab089835d8a9bc68b019aaf343d600
egm <egmpawelnowicki@gmail.com>
2019-12-13 20:34:55 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log('siema');
+const name = "Paweł";
+const age = 39;
+let pawel = null;
+console.log(name);
+console.log(age);
+console.log(pawel);
+console.log(`Nazywam się ${name} i mam ${age} lat`);
+
+let a = 10;
+console.log(a * 2);
